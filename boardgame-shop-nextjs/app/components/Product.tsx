702d6a6ece85861bcb420d926cc29ec12bcba3cb
reuterShawn <reutershawn45@gmail.com>
2024-05-10 21:18:20 -0500

    Create product and shop files

---
+++ boardgame-shop-nextjs/app/components/Product.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+// const Product = ({product}) => {
+//   return (
+//     <div className='border rounded-lg p-4 shadow-md'>
+//       <h2 className='text-lg font-semibold'>{product.name}</h2>
+//       <p className='text-gray-400'>${product.price.toFixed(2)}</p>
+//       <button></button>
+//     </div>
+//   )
+// }
+
+// export default Product
