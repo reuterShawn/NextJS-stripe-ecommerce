183aaeb91cb50ecc747e7fea2b193d285c242328
reuterShawn <reutershawn45@gmail.com>
2024-05-10 21:11:52 -0500

    Initialize app

---
+++ boardgame-shop-nextjs/tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+
+
+  plugins: [require('daisyui')],
+  theme: {
+    daisyui: {
+      themes: [
+        "light", "dark", "business",
+
+      ],
+    },
+  },
+};
+export default config;
