539c33421bc17619ed8f65648caefa8b01ae38fa
reuterShawn <reutershawn45@gmail.com>
2024-05-14 23:11:46 -0500

    Format header

--- boardgame-shop-nextjs/app/components/Header.tsx
+++ boardgame-shop-nextjs/app/components/Header.tsx
@@ -1,28 +1,26 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
   return (
-
-    <section className='flex justify-content p-5 space-x-4'>
-        <div>
-            <Link href="/">Home</Link>
-        </div>
-        <div>
-            <Link href="/games">Games</Link>
-        </div>
-        <div>
-            <Link href="/about">About</Link>
-        </div>
-        <div>
-            <Link href="/shop">Shop</Link>
-        </div>
-        <div>
-            <Link href="/blog">Blog</Link>
-        </div>
-
+    <section className="flex justify-content p-5 space-x-4">
+      <div>
+        <Link href="/">Home</Link>
+      </div>
+      <div>
+        <Link href="/games">About</Link>
+      </div>
+      <div>
+        <Link href="/about">About</Link>
+      </div>
+      <div>
+        <Link href="/shop">Shop</Link>
+      </div>
+      <div>
+        <Link href="/blog">Blog</Link>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
