246f5c8b244931dd942ebb34df3326573184a1b2
reuterShawn <reutershawn45@gmail.com>
2024-05-14 23:12:16 -0500

    Add button

--- boardgame-shop-nextjs/app/(marketing)/about/page.tsx
+++ boardgame-shop-nextjs/app/(marketing)/about/page.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
-  )
+    <><div>page</div>
+<button className="btn btn-primary">Button</button>  
+</>
+)
 }
 
 export default page
