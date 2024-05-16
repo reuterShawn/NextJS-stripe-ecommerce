fda2b17155c2115e4023ae919539a9425675e6e1
reuterShawn <reutershawn45@gmail.com>
2024-05-15 22:46:54 -0500

    Add pages

--- boardgame-shop-nextjs/app/page.tsx
+++ boardgame-shop-nextjs/app/page.tsx
@@ -1,10 +1,29 @@
+
+import React from 'react'
 import Image from "next/image";
+import { getCategorizedArticles } from "./lib/articles";
+import ArticleItemList from "./components/ArticleListItem";
 
-export default function Home() {
+const page = () => {
+
+    const articles = getCategorizedArticles()
+
   return (
-    <><html data-theme="retro"></html><main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-
-    </main></>
-  );
+    <><html></html><section className="mx-auto w-11/12 md:2-1/2 mt-20 flex flex-col gap-16">
+      <header className="font-cormorantGaramond font-light text-6xl text-neutral-900 text-center">
+        <h1>Board Game Blog</h1>
+      </header>
+      <section className="md:grid md:grid-col-2 flex flex-col gap-10">
+        {articles !== null &&
+          Object.keys(articles).map((article) => (
+            <ArticleItemList
+              category={article}
+              articles={articles[article]}
+              key={article} />
+          ))}
+      </section>
+    </section></>
+  )
 }
+
+export default page
